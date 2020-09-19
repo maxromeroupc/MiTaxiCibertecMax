3aea35a557f184ceda9e869b079ea9f4a7296b65
Max Romero <maxrichard.1989@gmail.com>
2020-09-19 12:48:39 -0500

    Fin clase 4

--- app/src/main/java/com/example/mitaxicibertecmax/view/HomeActivity.java
+++ app/src/main/java/com/example/mitaxicibertecmax/view/HomeActivity.java
@@ -3,11 +3,24 @@ package com.example.mitaxicibertecmax.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.*;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.mitaxicibertecmax.R;
+
+/*
+Clave para firebase
+Variant: debug
+Config: debug
+Store: C:\Users\SUITE\.android\debug.keystore
+Alias: AndroidDebugKey
+MD5: A6:35:06:C9:56:40:E2:AA:F5:4E:EA:7F:E4:AE:27:9D
+SHA1: B3:0E:2B:B6:5E:E5:C1:D4:99:3A:3A:FD:58:BF:37:FB:A3:39:98:31
+SHA-256: 8F:17:28:9F:B9:57:AE:32:B6:CB:CE:00:C8:E1:BF:03:DF:35:2B:69:6C:66:F2:E2:8E:8D:24:CE:5F:E0:23:EE
+
+ */
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +39,21 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDriver:
+                    goToSelect();
+                break;
+            case R.id.btnClient:
+                    goToSelect();
+                break;
+
+        }
 
     }
+
+    private void goToSelect(){
+        Intent intentSelect = new Intent( this, SelectOptionAuthActivity.class );
+        startActivityForResult(intentSelect,0);
+    }
+
 }
--- app/src/main/java/com/example/mitaxicibertecmax/view/SelectOptionAuthActivity.java
+++ app/src/main/java/com/example/mitaxicibertecmax/view/SelectOptionAuthActivity.java
@@ -3,14 +3,29 @@ package com.example.mitaxicibertecmax.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mitaxicibertecmax.R;
 
-public class SelectOptionAuthActivity extends AppCompatActivity {
+public class SelectOptionAuthActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnIniciar, btnRegisterAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        btnIniciar = findViewById(R.id.btnIniciar);
+        btnRegisterAccount = findViewById(R.id.btnRegisterAccount);
+        btnIniciar.setOnClickListener(this);
+        btnRegisterAccount.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
